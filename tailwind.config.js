4bab5baea84f90c17d9fcc46fd019086a2989222
Nandhakumar <nknandhakumar64@gmail.com>
2024-06-14 17:34:40 +0530

    Usefull update : how to create a own animation in tailwind configuaration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
-import animations from '@midudev/tailwind-animations' 
+const animations = require('@midudev/tailwind-animations');
+
 module.exports = {
-  content: [ "./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        upDown: 'upDown 2s linear infinite',
+        spinClockwise: 'spinClockwise 5s linear infinite',
+      },
+      keyframes: {
+        upDown: {
+          '0%': { paddingTop: '0px' },
+          '50%': { paddingTop: '20px' },
+          '100%': { paddingTop: '0px' },
+        },
+        spinClockwise: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+    },
   },
   plugins: [animations],
-}
+};
